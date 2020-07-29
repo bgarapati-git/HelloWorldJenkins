f82093d62cf604ee5b08275185b30e126786ad95
bgarapati-git <58849852+bgarapati-git@users.noreply.github.com>
2020-07-29 15:59:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 public static void main(String[] args){
-	System.out.println("Hello World ***** Badari Garapati made changeds*****");
+	System.out.println("Hello World ***** Badari Garapati*****");
 	//System.out.println("*********New changes made to test Jenkins Poll SCM feature******");
-	System.out.println("*****Trying Webhook testing*****");
+	System.out.println("****Web Hook Demo*****");
 }
 }
